a149a2d20965bc2ad11fbf8fd4979b534ccd3e74
Héctor C. Alzate <halzate93@gmail.com>
2016-04-09 12:44:36 -0500

    Fantastic start scene

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+	[SerializeField]
+	public float TranslationZ = 2;
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		Vector3 vector = new Vector3(0, 0, TranslationZ);
+		transform.Translate(vector * Time.deltaTime);
+	}
+}
