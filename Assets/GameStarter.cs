96f790300d9f38c7c38fb3aa8b0f90f06989f7ea
Héctor C. Alzate <halzate93@gmail.com>
2016-04-09 14:13:42 -0500

    Demo movement.

---
+++ Assets/GameStarter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+	public GameObject prefabToInstantiate;
+
+	public Transform initialTransform;
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+
+	private void InstantiateCharacter()
+	{
+		PhotonNetwork.Instantiate(prefabToInstantiate.name, initialTransform.position, initialTransform.rotation, 0);
+	}
+}
